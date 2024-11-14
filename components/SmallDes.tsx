c05a523a886c3ea642e72e72511700747ee7046b
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-11-14 19:39:01 +0600

    Update: Tweeking ui.

--- components/SmallDes.tsx
+++ components/SmallDes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const SmallDes: React.FC = () => {
   return (
-    <section className="text-pretty px-8 text-sm text-slate-500 sm:px-20 sm:text-center sm:text-base">
-      Join us for events that fuel creativity, innovation, and growth! Share
-      your ideas in the Poster Presentation, dive into expert-led Workshops,
-      tackle challenges in the Hackathon, unleash creativity in the Prompt
-      Challenge, and display your projects in the Competition. There’s something
-      here for every aspiring mind!
+    <section className="flex items-center justify-center">
+      <div className="w-full text-pretty sm:text-center px-8 text-sm text-slate-500 sm:max-w-7xl sm:px-20 sm:text-base">
+        Join us for events that fuel creativity, innovation, and growth! Share
+        your ideas in the Poster Presentation, dive into expert-led Workshops,
+        tackle challenges in the Hackathon, unleash creativity in the Prompt
+        Challenge, and display your projects in the Competition. There’s
+        something here for every aspiring mind!
+      </div>
     </section>
   );
 };
--- components/SummitGallery.tsx
+++ components/SummitGallery.tsx
@@ -6,9 +6,7 @@ import Image from "next/image";
 import { summits } from "@/constants";
 import TitleBar from "./Titlebar";
 
-
 const SummitGallery: React.FC = () => {
-
   return (
     <section className="mx-2 mt-10 max-w-full">
       <TitleBar title="Previous Summits" subText="text-3xl" />
@@ -19,13 +17,14 @@ const SummitGallery: React.FC = () => {
             <h2 className="text-center text-2xl font-semibold">
               {summit.title}
             </h2>
-            <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 p-2">
+            <div className="grid grid-cols-1 gap-2 p-2 sm:grid-cols-3">
               {summit.images.map((image, index) => (
                 <Image
-                  width={100}
-                  height={100}
+                  width={400}
+                  height={300}
                   key={index}
                   src={image}
+                  quality={100}
                   alt={`${summit.title} - ${index + 1}`}
                   className="h-80 w-full transform cursor-pointer rounded-lg object-contain shadow-md transition-transform duration-300 hover:scale-105"
                 />
