ae3a92069e48a2b50f60cc1c2a4bceb264aff2ec
Uthsob Chakraborty <uthsob9@gmail.com>
2024-12-09 02:53:51 +0600

    submit page

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
+import promptIMG from '@/public/prompt.jpg';
+import datathonIMG from '@/public/datahackathon.jpg'
 
 const Submit = () => {
-  return <section className="min-h-screen">Submit</section>;
+  return (
+    <div className="py-8 bg-gray-100">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Submit</h1>
+      <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+        <a href="/submit/promptcraft" className="max-w-sm rounded overflow-hidden shadow-lg bg-white transform hover:scale-105 transition-transform duration-300">
+          <div className="relative w-full h-48">
+            <Image className="object-cover" src={promptIMG} alt="PromptCraft Image" layout="fill" />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-center">PromptCraft</div>
+            <p className="text-gray-600 text-center">Join the ultimate platform to craft and share creative AI prompts.</p>
+          </div>
+        </a>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white transform hover:scale-105 transition-transform duration-300">
+          <div className="relative w-full h-48">
+            <Image className="object-cover" src={datathonIMG} alt="Data Hackathon Image" layout="fill" />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-center">Data Hackathon</div>
+            <p className="text-gray-600 text-center">Compete in solving real-world data challenges with innovative solutions.</p>
+          </div>
+        </div>
+      </div>
+    </div >
+  );
 };
 
 export default Submit;
