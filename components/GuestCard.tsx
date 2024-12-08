d591e0b11fdd27850797a3327ae855acf074724d
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-12-08 15:29:50 +0600

    Added: Chairman as a guest.

--- components/GuestCard.tsx
+++ components/GuestCard.tsx
@@ -8,15 +8,15 @@ interface Props {
 }
 
 const GuestCard: React.FC<Props> = ({ guest }) => {
-  const { name, img, designation, organization, category } = guest;
+  const { name, img, designation, organization } = guest;
 
   return (
     <div className="h-94 w-80 rounded-lg bg-white p-6 text-center shadow-md">
       <div className="mb-4 flex justify-center">
         <Image
           src={img}
-          width={300}
-          height={300}
+          width={330}
+          height={350}
           alt={name}
           className="object-fit h-64 w-64 rounded-sm"
         />
@@ -25,7 +25,7 @@ const GuestCard: React.FC<Props> = ({ guest }) => {
         <h3 className="text-lg font-semibold text-gray-800">{name}</h3>
         <p className="text-sm text-gray-600">{designation}</p>
         <p className="text-sm italic text-gray-500">{organization}</p>
-        {category === "chief" && (
+        {/* {category === "chief" && (
           <p className="font-bold text-red-800">Chief Guest</p>
         )}
         {category === "special" && (
@@ -33,7 +33,7 @@ const GuestCard: React.FC<Props> = ({ guest }) => {
         )}
         {category === "guest" && (
           <p className="font-bold text-green-500">Guest</p>
-        )}
+        )} */}
       </div>
     </div>
   );
