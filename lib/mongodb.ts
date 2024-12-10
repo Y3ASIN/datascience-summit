5537033a043e965f7df27633fa7766bc0418f7e2
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-12-10 21:15:59 +0600

    Fix: Vercel error.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,50 +1,17 @@
-import mongoose, { Connection } from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+let isConnected = false;
 
-if (!MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable');
-}
-
-// Extend the NodeJS Global type to include mongoose
-declare global {
-  namespace NodeJS {
-    interface Global {
-      mongoose?: {
-        conn: Connection | null;
-        promise: Promise<Connection> | null;
-      };
-    }
+export const connectToDatabase = async () => {
+  if (isConnected) {
+    return;
   }
-}
 
-// Use a type-safe global variable for Mongoose
-const globalForMongoose = global as typeof global & {
-  mongoose?: {
-    conn: Connection | null;
-    promise: Promise<Connection> | null;
-  };
+  try {
+    await mongoose.connect(String(process.env.MONGO_URI));
+
+    isConnected = true;
+  } catch (error) {
+    console.error(error);
+  }
 };
-
-// Initialize global.mongoose if not already initialized
-if (!globalForMongoose.mongoose) {
-  globalForMongoose.mongoose = { conn: null, promise: null };
-}
-
-export async function connectToDatabase(): Promise<Connection> {
-  // If a connection already exists, return it
-  if (globalForMongoose.mongoose!.conn) {
-    return globalForMongoose.mongoose!.conn;
-  }
-
-  // Otherwise, create a new promise for the connection
-  if (!globalForMongoose.mongoose!.promise) {
-    globalForMongoose.mongoose!.promise = mongoose
-      .connect(MONGO_URI as string) // Options removed as they are now defaults
-      .then((m) => m.connection);
-  }
-
-  // Wait for the promise to resolve and store the connection
-  globalForMongoose.mongoose!.conn = await globalForMongoose.mongoose!.promise;
-  return globalForMongoose.mongoose!.conn;
-}
